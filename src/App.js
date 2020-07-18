c58aeaada4b5f745a8ea9381d6d2171fd659fe12
Attila <59293104+sweeptic@users.noreply.github.com>
2020-07-18 16:59:06 +0200

    10. Transition Events

--- src/App.js
+++ src/App.js
@@ -52,7 +52,15 @@ class App extends Component {
           in={this.state.showBlock}
           timeout={300}
           mountOnEnter
-          unmountOnExit>
+          unmountOnExit
+          onEnter={() => console.log('onEnter')}
+          onEntering={() => console.log('onEntering')}
+          onEntered={() => console.log('onEntered')}
+          onExit={() => console.log('onExit')}
+          onExiting={() => console.log('onExiting')}
+          onExited={() => console.log('onExited')}
+        >
+
           {state => (
 
             <div
